2660fa64351160fe921dcad8b10de1827b65089d
YoloChocoPie <chocopieyolo@gmail.com>
2019-11-19 18:18:47 +0700

    Student Management

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/CreateClassForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class CreateClassForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(31, 24);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(68, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Class\'s name";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(31, 58);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(60, 13);
+            this.lblDescription.TabIndex = 1;
+            this.lblDescription.Text = "Description";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(42, 92);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(135, 92);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(110, 55);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(100, 20);
+            this.txtDescription.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(110, 21);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // CreateClassForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 132);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblName);
+            this.Name = "CreateClassForm";
+            this.Text = "CreateClassForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/IndexClassForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class IndexClassForm : Form
+    {
+        private ClassManagement Business;
+        public IndexClassForm()
+        {
+            InitializeComponent();
+            this.Business = new ClassManagement();
+            this.Load += IndexClassForm_Load;
+            this.btnAddClass.Click += btnAddClass_Click;
+            this.btnDelete.Click += btnDelete_Click;
+            this.grdClasses.DoubleClick += grdClasses_DoubleClick;
+        }
+
+        void grdClasses_DoubleClick(object sender, EventArgs e)
+        {
+            var @class = (Class)this.grdClasses.SelectedRows[0].DataBoundItem;
+            var updateForm = new UpdateClassForm(@class.id);
+            updateForm.ShowDialog();
+            this.LoadAllClasses();
+
+        }
+
+        void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (this.grdClasses.SelectedRows.Count == 1)
+            {
+                if (MessageBox.Show("Do you want to delete this?", "Comfirm", MessageBoxButtons.YesNo)
+                    == System.Windows.Forms.DialogResult.Yes)
+                {
+                    var @class = (Class)this.grdClasses.SelectedRows[0].DataBoundItem;
+                    this.Business.DeleteClass(@class.id);
+                    MessageBox.Show("Delete class successfully.");
+                    this.LoadAllClasses();
+                }
+            }
+        }
+
+        void btnAddClass_Click(object sender, EventArgs e)
+        {
+            var createform = new CreateClassForm();
+            createform.ShowDialog();
+            this.LoadAllClasses();
+        }
+
+        void IndexClassForm_Load(object sender, EventArgs e)
+        {
+            this.LoadAllClasses();
+        }
+
+        private void LoadAllClasses()
+        {
+            var classes = this.Business.GetClasses();
+            this.grdClasses.DataSource = classes;
+        }
+    }
+}
